34ee3f9491e32d82422784b7c7adb00ba3738b02
Rafael Cabañas de Paz <rafacabanas@gmail.com>
2017-12-07 13:52:35 +0100

    scroll reveals

---
+++ docs/_build/html/_static/sr_settings.js
@@ -0,0 +1,33 @@
+// GENERAL SETTING
+window.sr = ScrollReveal({ reset: true });
+
+// Custom Settings
+sr.reveal('.areas-block', { duration: 500 });
+sr.reveal('.facts-block', { duration: 500 });
+sr.reveal('.software-block', { duration: 500 });
+sr.reveal('.consulting-block', { duration: 500 });
+sr.reveal('.success-block', { duration: 500 });
+sr.reveal('.partners-block', { duration: 500 });
+
+
+
+
+/* sr.reveal('facts-block', { 
+  origin: 'right', 
+  duration: 2000 
+});
+
+sr.reveal('.foo-3', { 
+  rotate: { x: 100, y: 100, z: 0 },
+  duration: 1000
+});
+
+sr.reveal('.foo-4', { 
+  viewFactor: 0.5
+});
+
+sr.reveal('.foo-5', { 
+  duration: 200 
+});
+
+*/
